5e5e5f1f359ac1ca92489761081438aa1d1eb4b2
MXCode <maxsimakov123@yandex.ru>
2022-03-17 14:26:18 +0400

    main

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from './comp/header.jsx'
+import Slides from './comp/slides.jsx'
 import ReactDOM from 'react-dom';
-import Widget from './widget.js';
+import './styles/widget.css'
+
+
+function Widget(){
+    const [slide, setSlide] = useState(0);
+
+    function nextSlide(){
+        setSlide(slide+1);
+    }
+
+    function previousSlide(){
+        setSlide(slide-1);
+    }
+
+    function zeroSlide(){
+        setSlide(slide-slide);
+    }
+
+    return(
+        <div className="widget">
+            <Header slide={slide} nextSlide={nextSlide}  previousSlide={previousSlide}  zeroSlide={zeroSlide}/>
+            <Slides slide={slide} nextSlide={nextSlide}/>
+        </div>
+    )
+}
+
+
 
 ReactDOM.render(
-    <Widget/>,
-  document.getElementById('root')
+    Widget(),
+    document.getElementById('glasses-quiz-widget')
 );
